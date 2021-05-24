68cb2e9caaf762048c59927f7ba4e0fe4f8b0d98
jjh7059 <wlswngus2@naver.com>
2021-05-24 12:51:54 +0900

    handler...

--- src/xyz/itwill/event/ActionCommandApp.java
+++ src/xyz/itwill/event/ActionCommandApp.java
@@ -1,13 +1,60 @@
 package xyz.itwill.event;
 
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Frame;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 //색상 버튼을 누른 경우 캔버스의 배경색을 변경하는 프로그램
 public class ActionCommandApp extends Frame{
 	private static final long serialVersionUID = 1L;
 	
+	//이벤트 핸들러 메소드에서 사용할 컴퍼넌트 또는 컨테이너는 필드로 선언
+	Button red, green, blue, white;
+	Canvas canvas;
+	
 	public ActionCommandApp(String title) {
 		super(title);
+		
+		red = new Button("빨간색");
+		green = new Button("초록색");
+		blue = new Button("파란색");
+		white = new Button("하얀색");
+		
+		Panel panel = new Panel();
+		panel.add(red);
+		panel.add(green);
+		panel.add(blue);
+		panel.add(white);
+		
+		canvas = new Canvas();
+		
+		add(panel, BorderLayout.NORTH);
+		add(canvas, BorderLayout.CENTER);
+		
+		panel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
+		panel.setBackground(Color.GRAY);
+		
+		white.setEnabled(false);
+		
+		red.addActionListener(new RedButtonEventHandler());
+		green.addActionListener(new GreenButtonEventHandler());
+		blue.addActionListener(new BlueButtonEventHandler());
+		white.addActionListener(new WhiteButtonEventHandler());
+		
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
 		
 		setBounds(800, 200, 400, 400);
 		setVisible(true);
@@ -16,5 +63,53 @@ public class ActionCommandApp extends Frame{
 	
 	public static void main(String[] args) {
 		new ActionCommandApp("ActionCommand");
+	}
+	
+	//모든 색상 버튼을 활성화 처리하는 메소드 - 초기화
+	public void initColorButton() {
+		red.setEnabled(true);
+		green.setEnabled(true);
+		blue.setEnabled(true);
+		white.setEnabled(true);
+	}
+	
+	public class RedButtonEventHandler implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			initColorButton();
+			red.setEnabled(false);
+			
+			canvas.setBackground(Color.RED);
+		}
+	}
+	
+	public class GreenButtonEventHandler implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			initColorButton();
+			green.setEnabled(false);
+			
+			canvas.setBackground(Color.GREEN);
+		}
+	}
+	
+	public class BlueButtonEventHandler implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			initColorButton();
+			blue.setEnabled(false);
+
+			canvas.setBackground(Color.BLUE);
+		}
+	}
+	
+	public class WhiteButtonEventHandler implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			initColorButton();
+			white.setEnabled(false);
+			
+			canvas.setBackground(Color.WHITE);
+		}
 	}
 }
