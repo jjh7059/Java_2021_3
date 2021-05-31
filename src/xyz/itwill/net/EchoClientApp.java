6da3f1f0434d57ebfb0a944c1393f098e53c5259
jjh7059 <wlswngus2@naver.com>
2021-05-31 17:32:55 +0900

    TCP, UDP Transport...

---
+++ src/xyz/itwill/net/EchoClientApp.java
@@ -0,0 +1,40 @@
+package xyz.itwill.net;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+//키보드로 메시지를 입력받아 서버에 접속하여 접속 서버에 메시지를 전달하는 클라이언트 프로그램
+public class EchoClientApp {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print("전달 메시지 입력 >> ");
+		String message = in.readLine();
+		
+		try {
+			//서버 접속
+			Socket socket = new Socket("localhost", 4000);
+			
+			//소켓의 출력 스트림을 반환받아 문자열을 전달할 수 있도록 확장
+			/*
+			BufferedWriter out = new BufferedWriter
+					(new OutputStreamWriter(socket.getOutputStream()));
+			
+			out.write(message);
+			out.flush();
+			*/
+			
+			PrintWriter out = new PrintWriter(socket.getOutputStream());
+			out.println(message);
+			out.flush();
+			
+			socket.close();
+			
+		} catch (IOException e) {
+			System.out.println("[에러]서버에 접속할 수 없습니다.");
+		}
+	}
+}
